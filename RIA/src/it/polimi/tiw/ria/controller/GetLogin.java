3f75f9e5b1b6e66e7d6372f122e4d6894fec7afd
Marco Petri <marco.petri@mail.polimi.it>
2020-07-11 14:17:06 +0200

    Login template without javascript

---
+++ RIA/src/it/polimi/tiw/ria/controller/GetLogin.java
@@ -0,0 +1,30 @@
+package it.polimi.tiw.ria.controller;
+
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ResourceBundle;
+
+@WebServlet("/index")
+public class GetLogin extends HttpServletThymeleaf {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		ResourceBundle lang = findLanguage(req);
+		ServletContext context = getServletContext();
+		WebContext webContext = new WebContext(req,resp,context);
+		String page = "login";
+
+		webContext.setVariable("lang",lang);
+		thymeleaf.process(page,webContext,resp.getWriter());
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/CheckLogin").forward(req,resp);
+	}
+}
---
+++ RIA/src/it/polimi/tiw/ria/controller/HttpServletThymeleaf.java
@@ -0,0 +1,70 @@
+package it.polimi.tiw.ria.controller;
+
+import it.polimi.tiw.ria.filter.HttpServletFilter;
+import it.polimi.tiw.ria.utils.Const;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.UnavailableException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class HttpServletThymeleaf extends HttpServlet {
+	protected Connection conn;
+	protected TemplateEngine thymeleaf;
+
+	@Override
+	public void init() throws ServletException {
+		try {
+			// getting the connection
+			ServletContext context = getServletContext();
+			conn = HttpServletFilter.applyConnection(context);
+
+			// preparing thymeleaf template
+			ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(context);
+			templateResolver.setTemplateMode(TemplateMode.HTML);
+			templateResolver.setCharacterEncoding("UTF-8");
+			templateResolver.setPrefix("/WEB-INF/");
+			templateResolver.setSuffix(".html");
+			thymeleaf = new TemplateEngine();
+			thymeleaf.setTemplateResolver(templateResolver);
+		} catch (ClassNotFoundException e) {
+			throw new UnavailableException(Const.unavailableException);
+		} catch (SQLException e) {
+			throw new UnavailableException(Const.sqlException);
+		}
+	}
+
+	@Override
+	public void destroy() {
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException ignored) {}
+	}
+
+	public static ResourceBundle findLanguage(HttpServletRequest req) {
+		ResourceBundle lang;
+
+		if (Const.acceptedLangTags.contains(req.getLocale().getLanguage())) {
+			String language = req.getLocale().getLanguage();
+			String country = Const.isoTagToCountry.get(language);
+			lang = ResourceBundle.getBundle(Const.propertiesBaseName,new Locale(language,country));
+		} else if (Const.acceptedOldIsoLangTags.contains(req.getLocale().getLanguage())) {
+			String language = Const.oldIsoLangTagsToNew.get(req.getLocale().getLanguage());
+			String country = Const.isoTagToCountry.get(language);
+			lang = ResourceBundle.getBundle(Const.propertiesBaseName,new Locale(language,country));
+		} else {
+			lang = ResourceBundle.getBundle(Const.propertiesBaseName,new Locale(Const.defaultLanguage,Const.defaultCountry));
+		}
+		return lang;
+	}
+}
