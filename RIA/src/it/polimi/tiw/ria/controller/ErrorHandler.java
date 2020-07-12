72ba227db44bf8ede3386f4210d9fb43fb719679
Marco Petri <marco.petri@mail.polimi.it>
2020-07-12 12:33:52 +0200

    RIA Error handler for errors

---
+++ RIA/src/it/polimi/tiw/ria/controller/ErrorHandler.java
@@ -0,0 +1,26 @@
+package it.polimi.tiw.ria.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/errorHandler")
+public class ErrorHandler extends HttpServlet {
+	@Override
+	public void init() throws ServletException {
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Integer statusCode = (Integer) req.getAttribute("javax.servlet.error.status_code");
+		resp.sendRedirect("/error?code="+statusCode);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
